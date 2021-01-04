6ab2a4422d3859c11f023e6c3f1e5508445a15f3
Nicholas Trujillo <nicholasrtrujillo@gmail.com>
2021-01-04 11:56:58 -0800

    basic setup complete

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,63 @@
-import '../styles/globals.css'
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
+import { Global, css } from "@emotion/react";
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { AuthProvider } from "@/utils/auth";
+
+const colors = {
+  brand: {
+    900: "#1a365d",
+    800: "#153e75",
+    700: "#2a69ac",
+  },
+};
+const fonts = {
+  body: `Inter,-apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"`,
+};
+const fontWeights = {
+  normal: 400,
+  medium: 600,
+  bold: 700,
+};
+const theme = extendTheme({
+  colors,
+  fonts,
+  fontWeights,
+});
+
+const GlobalStyle = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <meta content='width=device-width, initial-scale=1' name='viewport' />
+      </Head>
+      <Global
+        styles={css`
+          html {
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+      />
+      {children}
+    </>
+  );
+};
+
+function App({ Component, pageProps }) {
+  return (
+    <ChakraProvider theme={theme}>
+      <AuthProvider>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default App;
--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,38 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import { Button, Heading, Text, Code, useColorMode } from "@chakra-ui/react";
+
+import { useAuth } from "@/utils/auth";
 
 export default function Home() {
+  const auth = useAuth();
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  // console.log(user);
+
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
-        <link rel='icon' href='/favicon.ico' />
+        <title>Feedback Farm</title>
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>Feedback Farm</h1>
-
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href='https://nextjs.org/docs' className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href='https://nextjs.org/learn' className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href='https://github.com/vercel/next.js/tree/master/examples'
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href='https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      <main>
+        <Heading>
+          Feedback Farm{" "}
+          <Button onClick={toggleColorMode}>
+            Toggle {colorMode === "light" ? "Dark" : "Light"}
+          </Button>
+        </Heading>
+        {auth.user ? (
+          <div>
+            <Text>
+              Email: <Code>{auth.user.email}</Code>
+            </Text>
+            <Button onClick={(e) => auth.signout()}>Sign Out</Button>
+          </div>
+        ) : (
+          <Button onClick={(e) => auth.signinWithGitHub()}>Sign In</Button>
+        )}
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Powered by{" "}
-          <img src='/vercel.svg' alt='Vercel Logo' className={styles.logo} />
-        </a>
-      </footer>
     </div>
   );
 }
