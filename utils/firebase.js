7be1d6b2946e200a9be8d073ae347acb8a2cdeb7
Nicholas Trujillo <nicholasrtrujillo@gmail.com>
2021-02-20 22:52:52 -0800

    add private key to firebase.js

--- utils/firebase.js
+++ utils/firebase.js
@@ -8,6 +8,7 @@ if (!firebase.apps.length) {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY,
   });
 }
 
