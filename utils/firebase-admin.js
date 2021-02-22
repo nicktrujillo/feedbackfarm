d634a7c0eb1934d7ff598f10602b48c87368fd71
Nicholas Trujillo <nicholasrtrujillo@gmail.com>
2021-02-21 23:02:18 -0800

    attempt to fix private key issue

--- utils/firebase-admin.js
+++ utils/firebase-admin.js
@@ -4,7 +4,7 @@ if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
       project_id: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-      private_key: process.env.FIREBASE_PRIVATE_KEY,
+      private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
       client_email: process.env.FIREBASE_CLIENT_EMAIL,
     }),
     databaseURL: "feedback-farm.firebaseapp.com",
